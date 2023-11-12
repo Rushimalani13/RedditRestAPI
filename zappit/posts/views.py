7d89e8e71c5b88cc8311d768ef21266a670252eb
Rushikesh Malani <rushikeshmalani66@gmail.com>
2023-11-12 12:18:26 +0530

    First Rest API

---
+++ zappit/posts/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+from rest_framework import generics, permissions, mixins, status
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from .serializers import PostSerializer,VoteSerializer
+from .models import Post,Vote
+# Create your views here.
+class PostList(generics.ListCreateAPIView):
+    queryset=Post.objects.all()
+    serializer_class=PostSerializer
+    permission_classes=[permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self,serializer):
+        serializer.save(poster=self.request.user)
+
+class PostRetrieveDestroy(generics.RetrieveDestroyAPIView):
+    queryset=Post.objects.all()
+    serializer_class=PostSerializer
+    permission_classes=[permissions.IsAuthenticatedOrReadOnly]
+
+    def delete(self,request,*args, **kwargs):
+        post= Post.objects.filter(pk=kwargs['pk'],poster=self.request.user)
+        if post.exists():
+            return self.destroy(request, *args, **kwargs) 
+        else:
+            raise ValidationError('this is not a valid')
+
+
+    
+class VoteCreate(generics.CreateAPIView,mixins.DestroyModelMixin):
+    serializer_class=VoteSerializer
+    permission_classes=[permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user=self.request.user
+        post=Post.objects.get(pk=self.kwargs['pk'])
+        return Vote.objects.filter(voter=user,post=post)    
+    
+    def perform_create(self,serializer):
+        if self.get_queryset().exists():
+            raise ValidationError('You already have Voted')
+        serializer.save(voter=self.request.user,post=Post.objects.get(pk=self.kwargs['pk']))
+
+    def delete(self,*args, **kwargs):
+        if self.get_queryset().exists():
+            self.get_queryset().delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            raise ValidationError("you never vote!")
